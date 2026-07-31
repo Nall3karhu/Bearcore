6f6e75d6db197185144070b63d42eed60954a7c7
Nall3karhu <mikael.helenius98@gmail.com>
2026-08-01 02:09:15 +0300

    Added feature request and request engine

---
+++ tests/test_feature_request.py
@@ -0,0 +1,6 @@
+from modules.feature_request.feature_request import feature_request
+
+
+def test_feature_request():
+
+    assert feature_request() == True
---
+++ tests/test_request_engine.py
@@ -0,0 +1,6 @@
+from modules.request_engine.request_engine import request_engine
+
+
+def test_request_engine():
+
+    assert request_engine() == True
